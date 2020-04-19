dcfd38b72688d46da8fea4d036779d63ccb45ffd
Himanshu Sharma <him.hss2791@gmail.com>
2020-04-19 13:31:48 +0530

    New constant file is added

---
+++ MyFirstGithubApp/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  MyFirstGithubApp
+//
+//  Created by Himanshu Sharma on 19/04/20.
+//  Copyright © 2020 Himanshu Sharma. All rights reserved.
+//
+
+import UIKit
+
+class Constant: NSObject {
+
+}
