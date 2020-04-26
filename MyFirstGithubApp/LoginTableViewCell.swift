74ebfdfc20362543742844a61c0038fd88b99322
Himanshu Sharma <him.hss2791@gmail.com>
2020-04-26 17:27:38 +0530

    Pod Project is created

---
+++ MyFirstGithubApp/LoginTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoginTableViewCell.swift
+//  MyFirstGithubApp
+//
+//  Created by Himanshu Sharma on 19/04/20.
+//  Copyright © 2020 Himanshu Sharma. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
